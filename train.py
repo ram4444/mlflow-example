99a293d8bad95c313463516ad070d3960e6a9394
Ram Lau <ramwt4444@gmail.com>
2023-09-03 03:42:53 +0100

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 
 import mlflow
-mlflow.set_tracking_uri(trackinguri)
+#mlflow.set_tracking_uri(trackinguri)
 #mlflow.autolog()
 import mlflow.sklearn
 
