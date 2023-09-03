7d7696c1ba412e115fff6f9cfa3145402c428769
Ram Lau <ramwt4444@gmail.com>
2023-09-03 02:58:45 +0100

    Add tracking uri

--- train.py
+++ train.py
@@ -43,6 +43,9 @@ if __name__ == "__main__":
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    trackinguri = sys.argv[3] if len(sys.argv) > 3 else "http://somewhere:5000"
+    mlflow.set_tracking_uri(trackinguri)
+    mlflow.autolog()
 
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
