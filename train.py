94140f8c70944c60267fabf7e18bb9dbf5ece43e
Ram Lau <ramwt4444@gmail.com>
2023-09-07 00:02:44 +0100

    Update train.py

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ import sys
 
 import pandas as pd
 import numpy as np
+import tensorflow_io as tfio
+from tensorflow import keras
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
