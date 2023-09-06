60761b237a010fa80bef6b5f16879c35d48f3b8a
Ram Lau <ramwt4444@gmail.com>
2023-09-07 00:29:35 +0100

    Update train.py

--- train.py
+++ train.py
@@ -59,23 +59,23 @@ if __name__ == "__main__":
         #run = mlflow.active_run()
         #print("Active run_id: {}".format(run.info.run_id))
         
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y, callbacks=[tensorboard_callback])
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    lr.fit(train_x, train_y, callbacks=[tensorboard_callback])
 
-        predicted_qualities = lr.predict(test_x)
+    predicted_qualities = lr.predict(test_x)
 
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
 
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
 
-        mlflow.sklearn.log_model(lr, "model")
+    mlflow.sklearn.log_model(lr, "model")
     #mlflow.end_run()
